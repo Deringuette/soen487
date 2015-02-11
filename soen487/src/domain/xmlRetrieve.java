196202857483fe4e6e7491e69dd99d776c6ebc17
Guillaume Fortin <xguillaumex@hotmail.com>
2015-02-11 17:47:53 -0500

    all file names added

--- soen487/src/domain/xmlRetrieve.java
+++ soen487/src/domain/xmlRetrieve.java
@@ -6,6 +6,8 @@ public class xmlRetrieve {
 	public static void main (String[] args){
 		//XMLRetriever.requestXML("http://www.ledevoir.com/rss/ledevoir.xml", "soen487/XMLResources/rssNews.xml");
 		//XMLRetriever.requestXML("http://marf.cvs.sf.net/viewvc/marf/apps/TestNN/samples/test1.xml?revision=1.1", "soen487/XMLResources/marfSample1.xml");
-		XMLRetriever.requestXML("http://users.encs.concordia.ca/~s487_4/project/marfcat-in.xml", "soen487/XMLResources/marfcat-in.xml");
+		//XMLRetriever.requestXML("http://users.encs.concordia.ca/~s487_4/project/marfcat-in.xml", "soen487/XMLResources/marfcat-in.xml");
+		//XMLRetriever.requestXML("http://users.encs.concordia.ca/~s487_4/project/marfcat-out.xml", "soen487/XMLResources/marfcat-out.xml");
+		//XMLRetriever.requestXML("http://users.encs.concordia.ca/~s487_4/examples/soap/faultmessage/faultSample.wsdl", "soen487/XMLResources/faultSample.wsdl");
 	}
 }
